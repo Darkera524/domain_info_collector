4e49a3d42fb54030b5e20346aecf408fe1988c49
王一琦 <wangyiqi@gridsum.com>
2017-11-29 13:41:24 +0800

    Add the summary of the search type info

--- db/pg.go
+++ db/pg.go
@@ -6,9 +6,20 @@ import
 	"domain_info_collector/transfer"
 	"bytes"
 	"fmt"
+	"regexp"
+	"strings"
 )
 
+type single_normal_search struct{
+	callerIp string
+	searchType string
+	domainServer string
+	date string
+}
+
 func SendSearchInfoToPostgresql(searchList []*transfer.SearchInfo)(err error){
+	info_list := query_data(searchList)
+
 	config := initConfig()
 	conn, err := pgx.Connect(*config)
 	if err != nil {
@@ -67,7 +78,15 @@ func SendSearchInfoToPostgresql(searchList []*transfer.SearchInfo)(err error){
 	fmt.Println(sql)
 	_,err = tx.Exec(sql)
 	if err != nil {
-		fmt.Println("exec err")
+		fmt.Println("exec err1")
+		return err
+	}
+
+	sql = generate_write_sql(info_list)
+
+	_,err = tx.Exec(sql)
+	if err != nil {
+		fmt.Println("exec err2")
 		return err
 	}
 
@@ -148,6 +167,121 @@ func SendLdapSearchInfoToPostgresql(ldapList []*transfer.LdapRequest)(err error)
 
 
 	return nil
+}
+
+func query_data(origin_list []*transfer.SearchInfo) ([]*single_normal_search){
+	var info_list []*single_normal_search
+
+	for _,ins := range origin_list{
+		var single single_normal_search
+		if ignore(*ins){
+			continue
+		} else {
+			single.callerIp = ins.CallerIP
+			single.domainServer = ins.DomainServer
+			single.date = strings.Split(ins.TimeCreated,"T")[0]
+		}
+		if isSearchingUser(*ins){
+			single.searchType = "User"
+		} else if isSearchingGroup(*ins){
+			single.searchType = "Group"
+		} else if isSearchingByObjectUid(*ins){
+			single.searchType = "ObjectSid"
+		} else {
+			single.searchType = "Other"
+		}
+		info_list = append(info_list, &single)
+	}
+
+
+	return info_list
+}
+
+func ignore(search transfer.SearchInfo) bool {
+	//callerip正则表达式
+	ignoreReg := "(Internal|SAM|LSA|KCC|NTDSAPI|\\[)"
+
+	if match,_ := regexp.MatchString(ignoreReg, search.CallerIP);match{
+		return true
+	}
+	return false
+}
+
+func isSearchingUser(search transfer.SearchInfo) bool{
+	//filter正则表达式
+	filterReg := ".*(objectClass=user|displayName=|sAMAccountName=).*$"
+
+	//requiredattributes正则表达式
+	requiredAttributesReg := "(userAccountControl|memberOf|primaryGroupID|mail|userPrincipalName|facsimileTelephoneNumber|sn|givenName|pwdLastSet)"
+
+	//objdn正则表达式
+	objdnReg := "(OU=公司成员,OU=gridsum-members,DC=gridsum,DC=com)"
+
+	if match,_ := regexp.MatchString(filterReg, search.Filter);match{
+		return true
+	} else if match,_ := regexp.MatchString(requiredAttributesReg, search.RequiredAttributes);match{
+		return true
+	} else if match,_ := regexp.MatchString(objdnReg, search.ObjDN);match{
+		return true
+	}
+
+	return false
+}
+
+func isSearchingGroup(search transfer.SearchInfo) bool {
+	//filter正则表达式
+	filterReg := "(objectClass=group)"
+
+	//requiredattributes正则表达式 先行判断了memberOf，故此处不必担心
+	requiredAttributesReg := "(member)"
+
+	//objdn正则表达式
+	objdnReg := "(ou=group,ou=gridsum-members,dc=gridsum,dc=com)"
+
+	if match,_ := regexp.MatchString(filterReg, search.Filter);match{
+		return true
+	} else if match,_ := regexp.MatchString(requiredAttributesReg, search.RequiredAttributes);match{
+		return true
+	} else if match,_ := regexp.MatchString(objdnReg, search.ObjDN);match{
+		return true
+	}
+
+	return false
+}
+
+func isSearchingByObjectUid(search transfer.SearchInfo) bool {
+	//objectUid正则表达式
+	objectUidReg := "(objectSid)"
+
+	if match,_ := regexp.MatchString(objectUidReg, search.Filter);match{
+		return true
+	}
+
+	return false
+}
+
+func generate_write_sql(list []*single_normal_search) string {
+
+	var buffer bytes.Buffer
+	buffer.WriteString("INSERT INTO searchtype (callerip,searchtype,domainserver,date) VALUES ")
+
+	for i,search := range list{
+		buffer.WriteString("('")
+		buffer.WriteString(search.callerIp)
+		buffer.WriteString("','")
+		buffer.WriteString(search.searchType)
+		buffer.WriteString("','")
+		buffer.WriteString(search.domainServer)
+		buffer.WriteString("','")
+		buffer.WriteString(search.date)
+		buffer.WriteString("')")
+		if i+1 != len(list){
+			buffer.WriteString(",")
+		}
+	}
+	sql := buffer.String()
+
+	return sql
 }
 
 func initConfig() (config *pgx.ConnConfig){
